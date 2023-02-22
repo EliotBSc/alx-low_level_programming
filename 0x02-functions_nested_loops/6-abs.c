9aaca4058c461b3c8dc90768e80026e29ecdaf30
EliotBSc <eliotmalefofanebsc@gmail.com>
2023-02-22 02:55:38 -0800

    Computes absolute value of int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - calculates the absolute value
+ * @n: number to be converted to absolute value
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+if (n < 0)
+a *= -1;
+return (a);
+}

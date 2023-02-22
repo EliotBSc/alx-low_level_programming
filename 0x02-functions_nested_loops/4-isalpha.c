1043d93708390d6b3fb906a1bbac260a54aee19a
EliotBSc <eliotmalefofanebsc@gmail.com>
2023-02-22 02:24:04 -0800

    checks for alphabetic charecter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic charecter
+ * 
+ * @c: charecter to be tested
+ *
+ * Return 1 if @c is lowercase or uppercase, otherwise 0
+ */
+int _isalpha(char c)
+{
+if ((c >= 97 && c <= 122 || (c >= 65 && c <= 90))
+return (1);
+else
+return (0);
+}

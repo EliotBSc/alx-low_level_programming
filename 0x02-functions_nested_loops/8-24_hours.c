6d7b9bcb84754b6bb948bd014ed1c8ead4b1f711
EliotBSc <eliotmalefofanebsc@gmail.com>
2023-02-22 16:40:55 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day
+ * Return: void
+ */
+void jack_bauer(void)
+{
+int s = 0, e = 23, s2 = 0, e2 = 59;
+while (s <= e)
+{
+while (s2 <= e2)
+{
+_putchar(s < 9 ? 0 + '0' : s / 10 + '0');
+_putchar(s < 9 ? s + '0' : s % 10 + '0');
+_putchar(':');
+_putchar(s2 < 9 ? 0 + '0' : s / 10 + '0');
+_putchar(s2 < 9 ? s2 + '0' : s2 % 10 + '0');
+_putchar('\n');
+
+s2++;
+}
+s2 = 0;
+s++
+}
+}

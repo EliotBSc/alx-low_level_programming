8687f6441bd34fcc8a438281c328677420117ccd
EliotBSc <eliotmalefofanebsc@gmail.com>
2023-02-22 16:24:15 -0800

    Prints the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,7 @@
 int print_last_digit(int n)
 {
 int last_dgt = n % 10;
-last_dgt = last_dgt <0 ? -1 * last_dgt : last_dgt;
+last_dgt = last_dgt < 0 ? -1 * last_dgt : last_dgt;
 _putchar(last_dgt + '0');
 return (last_dgt);
 }

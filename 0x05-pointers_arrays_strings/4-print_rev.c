bc6223cc3ebe14493041c054704cff9650b04e83
EliotBSc <eliotmalefofanebsc@gmail.com>
2023-02-27 15:45:40 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - prints a reversed string
+ * @s String to be reversed 
+ */
+void print_rev(char *s)
+{
+int i, n;
+s = "hello";
+n = strlen(s);
+for (i = n - 1; i <= 0; i--)
+{
+_putchar(s[i]);
+}
+}
